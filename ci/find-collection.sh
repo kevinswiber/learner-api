ca7f5e8833573d1c6464e6bd336a575085cfe719
Kevin Swiber <kswiber@gmail.com>
2021-01-01 13:33:28 -0800

    Fixing filtering on API Version name for Version ID fetching in CI script.
    Signed-off-by: Kevin Swiber <kswiber@gmail.com>

--- ci/find-collection.sh
+++ ci/find-collection.sh
@@ -11,7 +11,7 @@ echo "api id: ${API_ID}"
 api_version_id=$(curl -s -H "X-API-Key: ${POSTMAN_API_KEY}" \
     "https://api.getpostman.com/apis/${API_ID}/versions" | \
     jq -r --arg API_VERSION_NAME "${api_version_name}" \
-    '.versions[] | select(.name == $API_VERSION_NAME) | .id')
+    '.versions[] | select(.name | contains($API_VERSION_NAME)) | .id')
 
 echo "api_version_id: ${api_version_id}"
 
