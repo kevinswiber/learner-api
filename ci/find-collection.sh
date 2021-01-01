0b793e894fdeb92381689afe4cbc9b3c26935eea
Kevin Swiber <kswiber@gmail.com>
2021-01-01 13:01:39 -0800

    Adding support for pr:### indicators in API version names.
    Signed-off-by: Kevin Swiber <kswiber@gmail.com>

--- ci/find-collection.sh
+++ ci/find-collection.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 
-[[ "$DEFAULT_BRANCH" != "$BRANCH_NAME" ]] && branch_id="branch: ${BRANCH_NAME}" || branch_id="${DEFAULT_API_VERSION}"
+[[ "$BRANCH_NAME" == "PR-"* ]] api_version_prefix = "branch" || api_version_prefix = "pr"
+[[ "$DEFAULT_BRANCH" != "$BRANCH_NAME" ]] && api_version_name="${api_version_prefix}:${BRANCH_NAME}" || api_version_name="${DEFAULT_API_VERSION}"
 
 echo "default branch: ${DEFAULT_BRANCH}"
 echo "branch name: ${BRANCH_NAME}"
-echo "branch id: ${branch_id}"
+echo "api version name: ${api_version_name}"
 echo "api id: ${API_ID}"
 
 api_version_id=$(curl -s -H "X-API-Key: ${POSTMAN_API_KEY}" \
     "https://api.getpostman.com/apis/${API_ID}/versions" | \
-    jq -r --arg BRANCH_ID "${branch_id}" \
-    '.versions[] | select(.name == $BRANCH_ID) | .id')
+    jq -r --arg API_VERSION_NAME "${api_version_name}" \
+    '.versions[] | select(.name == $API_VERSION_NAME) | .id')
 
 echo "api_version_id: ${api_version_id}"
 
@@ -27,8 +28,8 @@ echo "relation_id: ${relation_id}"
 # should check response and exit with error on non-200.
 curl -s -H "X-API-Key: ${POSTMAN_API_KEY}" \
     "https://api.getpostman.com/apis/${API_ID}/versions/${api_version_id}/integrationtest/${relation_id}" | \
-    jq --arg BRANCH_ID "${branch_id}" \
-    'select(.versionTag.name == $BRANCH_ID) | .collection' > postman_collection.json
+    jq --arg API_VERSION_NAME "${api_version_name}" \
+    'select(.versionTag.name | contains($API_VERSION_NAME)) | .collection' > postman_collection.json
 
 if [[ $(jq 'has("info")' ./postman_collection.json) == "true" ]]; then
     echo 'success: collection written'
