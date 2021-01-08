0e129248ed4617797b719833fdd7247d0475bf33
Kevin Swiber <kswiber@gmail.com>
2021-01-08 11:35:46 -0800

    Firming up CI script, falling back to current version if version tag isn't set.
    Signed-off-by: Kevin Swiber <kswiber@gmail.com>

--- ci/fetch-postman-assets.sh
+++ ci/fetch-postman-assets.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 if [[ -z "$JOB_NAME" ]]; then
     echo "JOB_NAME is required to execute this script."
@@ -15,17 +15,22 @@ if [[ -z "$BRANCH_NAME" ]]; then
     exit 1
 fi
 
-JOB_NAME="${JOB_NAME%/*}" # take only the first part of the job name
+job_label="job:${JOB_NAME%/*}" # take only the first part of the job name
 
 api_id=$(curl -s -H "X-API-Key: $POSTMAN_API_KEY" \
     "https://api.getpostman.com/apis" | \
-    jq -r --arg JOB_NAME "$JOB_NAME" \
-    '.apis[] | select(.name | contains($JOB_NAME)) | .id')
+    jq -r --arg JOB_NAME "$job_label" \
+    '.apis[] | select(.name | test("(^|\\s)" + $JOB_NAME + "($|\\s)")) | .id')
+
+if [[ -z "$api_id" ]]; then
+    >&2 echo "error: no api is tagged with the label $job_label"
+    exit 1
+fi
 
 default_api_version_name_and_branch=$(curl -s -H "X-API-Key: $POSTMAN_API_KEY" \
     "https://api.getpostman.com/apis/$api_id/versions" | \
     jq -r \
-    '[.versions[] | select(.name | contains("default:true")) | .name + "," + (.name | match("branch:(\\S+)") | .captures | map(.string)[0])][0]')
+    '[.versions[] | select(.name | test("(^|\\s)default:true($|\\s)")) | .name + "," + (.name | match("(?:^|\\s)branch:(\\S+)(?:$|\\s)") | .captures | map(.string)[0])][0]')
 
 default_api_version_name="${default_api_version_name_and_branch%,*}"
 git_default_branch_name="${default_api_version_name_and_branch##*,}"
@@ -33,17 +38,19 @@ git_default_branch_name="${default_api_version_name_and_branch##*,}"
 [[ $(expr "$BRANCH_NAME" : "PR-") != 0 ]] && api_version_prefix="pr" && BRANCH_NAME=$(echo "$BRANCH_NAME" | awk '{ print substr( $0, 4 ) }') || api_version_prefix="branch"
 [[ "$git_default_branch_name" != "$BRANCH_NAME" ]] && api_version_name="$api_version_prefix:$BRANCH_NAME" || api_version_name="$default_api_version_name"
 
-echo "job name: $JOB_NAME"
+echo "job name: ${JOB_NAME%/*}"
 echo "api id: $api_id"
 echo "default branch: $git_default_branch_name"
 echo "branch name: $BRANCH_NAME"
 echo "api version name: $api_version_name"
 
+[[ -f ./postman_collection.json ]] && rm ./postman_collection.json
+[[ -f ./postman_environment.json ]] && rm ./postman_environment.json
 
 api_version_id=$(curl -s -H "X-API-Key: $POSTMAN_API_KEY" \
     "https://api.getpostman.com/apis/$api_id/versions" | \
     jq -r --arg API_VERSION_NAME "$api_version_name" \
-    '.versions[] | select(.name | contains($API_VERSION_NAME)) | .id')
+    '.versions[] | select(.name | test("(^|\\s)" + $API_VERSION_NAME + "($|\\s)")) | .id')
 
 if [ -z "$api_version_id" ]; then
     echo "api version name not found, defaulting to: $default_api_version_name"
@@ -51,34 +58,51 @@ if [ -z "$api_version_id" ]; then
     api_version_id=$(curl -s -H "X-API-Key: $POSTMAN_API_KEY" \
         "https://api.getpostman.com/apis/$api_id/versions" | \
         jq -r --arg API_VERSION_NAME "$api_version_name" \
-        '.versions[] | select(.name | contains($API_VERSION_NAME)) | .id')
+        '.versions[] | select(.name | test("(^|\\s)" + $API_VERSION_NAME + "($|\\s)") | .id')
 fi
 
-echo "api_version_id: $api_version_id"
+echo "api version id: $api_version_id"
 
 integration_test_relation_id=$(curl -s -H "X-API-Key: $POSTMAN_API_KEY" \
     "https://api.getpostman.com/apis/$api_id/versions/$api_version_id/integrationtest" | \
     jq -r '.integrationtest[] | .id')
 
-echo "integration_test_relation_id: $integration_test_relation_id"
+echo "integration test relation id: $integration_test_relation_id"
 
 curl -s -H "X-API-Key: $POSTMAN_API_KEY" \
     "https://api.getpostman.com/apis/$api_id/versions/$api_version_id/integrationtest/$integration_test_relation_id" | \
     jq --arg API_VERSION_NAME "$api_version_name" \
-    'select(.versionTag.name | contains($API_VERSION_NAME)) | .collection' > postman_collection.json
+    'select(.versionTag.name | [$API_VERSION_NAME, "CURRENT"] | any)' > postman_version.tmp.json
 
-if [[ $(jq 'has("info")' ./postman_collection.json) == "true" ]]; then
-    echo 'success: collection written'
-else
-    echo "error: tagged collection not found.  Did you remember to tag the collection with the API Version?"
+if [[ $(wc -c ./postman_version.tmp.json | awk '{print $1}') == '0' ]]; then
+    >&2 echo "error: integration test not found on api version."
     exit 1
 fi
+
+tag=$(jq -r '.versionTag.name' ./postman_version.tmp.json)
+
+if [[ "$tag" == "CURRENT" ]]; then
+    echo "warning: tagged collection not found, falling back to CURRENT version of collection."
+fi
+
+echo "integration test version tag: $tag"
+
+jq '.collection' ./postman_version.tmp.json > ./postman_collection.json
+
+if [[ $(wc -c ./postman_collection.json | awk '{print $1}') == '0' ]]; then
+    >&2 echo "error: integration test collection not found."
+    exit 1
+fi
+
+rm ./postman_version.tmp.json
+
+echo 'success: collection written'
 
 environment_id=$(curl -s -H "X-API-Key: $POSTMAN_API_KEY" \
     "https://api.getpostman.com/apis/$api_id/versions/$api_version_id/environment" | \
     jq -r '.environment[] | .id')
 
-echo "environment_id: $environment_id"
+echo "environment id: $environment_id"
 curl -s -H "X-API-Key: $POSTMAN_API_KEY" \
     "https://api.getpostman.com/environments/$environment_id" | \
     jq '.environment' > postman_environment.json
